a121ffcfc4f7e630395d2fb6b6dc2cf3530fa5c2
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-05-06 23:18:53 +0300

    web.xml

---
+++ admin-service/src/main/java/com/homemade/admin/endpoints/AdminApp.java
@@ -0,0 +1,9 @@
+package com.homemade.admin.endpoints;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class AdminApp extends Application {
+    // This class doesn't need any implementation; just a marker for JAX-RS.
+}
