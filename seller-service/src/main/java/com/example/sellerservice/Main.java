47773583107cf4eff7dfd7032bcabd2128167925
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-05-07 21:24:57 +0300

    seller service

---
+++ seller-service/src/main/java/com/example/sellerservice/Main.java
@@ -0,0 +1,29 @@
+package com.example.sellerservice;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
+public class Main {
+    public static final String BASE_URI = "http://localhost:8081/api/";
+
+    // Start the server
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("com.example.sellerservice.api");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) {
+        final HttpServer server = startServer();
+        System.out.printf("Server started at %s%nPress Ctrl+C to stop...%n", BASE_URI);
+
+        // Keep server running
+        try {
+            Thread.currentThread().join(); // keep server alive
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
