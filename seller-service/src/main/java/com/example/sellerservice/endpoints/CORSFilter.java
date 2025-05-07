98372f6bbf8c820ffc576aed67d5716cea77f0d5
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-05-08 01:07:10 +0300

    create Dishes front

---
+++ seller-service/src/main/java/com/example/sellerservice/endpoints/CORSFilter.java
@@ -0,0 +1,16 @@
+package com.example.sellerservice.endpoints;
+import jakarta.ws.rs.container.*;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
