6813dbf42f6cccdce4b8f99bee7191b34adbae2a
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-05-09 19:58:25 +0300

    order publisher

--- customer-service/src/main/java/com/example/customerservice/OrderPublisher.java
+++ customer-service/src/main/java/com/example/customerservice/OrderPublisher.java
@@ -1,15 +1,22 @@
 package com.example.customerservice;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.*;
+
+import java.util.List;
 
 public class OrderPublisher {
 private static final String EXCHANGE_NAME = "orders";
-public static void placeOrder(Order order) {
+public static void placeOrder(List<Order> orders) {
     ConnectionFactory factory=new ConnectionFactory();
     factory.setHost("localhost");
     try(Connection connection=factory.newConnection();
         Channel channel= connection.createChannel()
     ){
         channel.exchangeDeclare(EXCHANGE_NAME,BuiltinExchangeType.FANOUT);
+        ObjectMapper mapper=new ObjectMapper();
+        String orderMessage=mapper.writeValueAsString(orders);
+        channel.basicPublish(EXCHANGE_NAME,"",null,orderMessage.getBytes());
+
             }
     catch(Exception e){
         e.printStackTrace();
